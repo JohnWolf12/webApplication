2bf63e0143b717a4c42241062df0b93027a0b20b
JohnWolf12 <82612279+JohnWolf12@users.noreply.github.com>
2023-04-18 23:10:36 +0200

    Rozszerzenie modelu użytkownika

--- aplikacjaogloszeniowa/models.py
+++ aplikacjaogloszeniowa/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Uzytkownik(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    telefon = models.CharField(max_length=9)
+    miejscowosc = models.CharField(max_length=50)
 
 
 class Kategoria(models.Model):
@@ -15,6 +22,7 @@ class Ogloszenie(models.Model):
     cena = models.DecimalField(max_digits=9, decimal_places=2)
     opis = models.TextField(blank=True, max_length=1000)
     podkategoria = models.ForeignKey(Podkategoria, on_delete=models.CASCADE)
+    uzytkownik = models.ForeignKey(Uzytkownik, on_delete=models.CASCADE)
 
 
 class Zdjecie(models.Model):
