bcb15c02381043cb439d9fadbf3e11892077ed80
JohnWolf12 <82612279+JohnWolf12@users.noreply.github.com>
2023-05-18 00:22:30 +0200

    Dodanie logowania

--- aplikacjaogloszeniowa/urls.py
+++ aplikacjaogloszeniowa/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('rejestracja/', views.register_view, name='rejestracja'),
+    path('logowanie/', views.login_view, name='logowanie'),
+    path('wylogowanie/', views.logout_view, name='wylogowanie'),
+
 ]
